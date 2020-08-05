b63c37c0306fb60ca2e34df9f48557df16f17d1d
Jessica <jessica.huang@silkrode.com.tw>
2020-08-05 19:00:30 +0800

    拆開兩個檔案

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jessica/development/flutter_sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jessica/playground/flutter_demo"
-export "FLUTTER_TARGET=/Users/jessica/playground/flutter_demo/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/jessica/playground/flutter-demo"
+export "FLUTTER_TARGET=/Users/jessica/playground/flutter-demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'src/Tabs.dart';
 
 class Screen extends StatelessWidget {
   Screen({this.current});
@@ -20,24 +21,6 @@ class Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return _widgetOptions.elementAt(current);
-  }
-}
-
-class Tabs extends StatelessWidget {
-  Tabs({this.current, this.onChangeTab});
-  final int current;
-  final Function onChangeTab;
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        currentIndex: current,
-        items: [
-          BottomNavigationBarItem(title: Text("Home"), icon: Icon(Icons.home)),
-          BottomNavigationBarItem(title: Text("List"), icon: Icon(Icons.list)),
-          BottomNavigationBarItem(
-              title: Text("Message"), icon: Icon(Icons.message)),
-        ],
-        onTap: onChangeTab);
   }
 }
 
